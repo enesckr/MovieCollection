93616478626890e8ff4d04dffd44ab92a6c21989
Enes ÇAKİR <62398743+enesckr@users.noreply.github.com>
2021-08-17 19:37:10 +0300

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def buildApp() {
+    sh "mvm clean install"
+    sh "mvn spring-boot:run"
+    echo 'building the application...'
+} 
+
+def testApp() {
+    echo 'testing the application...'
+} 
+
+def deployApp() {
+    echo 'deplying the application...'
+    echo "deploying version ${params.VERSION}"
+} 
+
+return this
